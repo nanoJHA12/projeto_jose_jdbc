395da557fe47efd41dc57e7a1065fe7e8a06f32a
nanoJHA12 <125267980+nanoJHA12@users.noreply.github.com>
2024-06-07 09:32:14 -0300

    Add files via upload

---
+++ src/main/java/com/castelo/App.java
@@ -0,0 +1,13 @@
+package com.castelo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
